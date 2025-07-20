57d462b1fd8a075b1a45986ce4c8ceef0ab08b7a
U Vivek Shenoy <vivekshenoy6763@gmail.com>
2025-07-20 16:51:06 +0530

    made the server ready

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import chatRoutes from "./routes/chat.js";
 import cors from "cors"
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// mongoose.connect(process.env)
 // Middleware
 app.use(express.json()); // to parse JSON bodies
 app.use(cors())
