aaf7652bdc80db8053489ea995927d2ed98ee488
U Vivek Shenoy <vivekshenoy6763@gmail.com>
2026-03-17 11:27:18 +0530

    cors error

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 // - Allows frontend (http://localhost:5173) to communicate with backend
 // - credentials: true allows cookies/sessions to be shared
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5174",
   credentials: true,
 }));
 
